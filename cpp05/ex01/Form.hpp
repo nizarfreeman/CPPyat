c47d82155b93b1cb69ce4adeae23352509ef9e51
nizarfreeman <gormarduck2005@gmail.com>
2026-02-16 22:25:42 +0000

    nf

---
+++ cpp05/ex01/Form.hpp
@@ -0,0 +1,15 @@
+#ifndef F_HPP
+# define F_HPP
+# include "Bureaucrat.hpp"
+
+class Form
+{
+    private:
+        const std::string name;
+        bool sign;
+        const int s_grade;
+        const int e_grade;
+    public:
+};
+
+#endif
