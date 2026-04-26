047c90fc88ecaa3834ba91a392ddcc9271824ea9
Nizar Faska <nfaska@e1r9p3.1337.ma>
2026-04-26 18:53:25 +0100

    nf

---
+++ cpp07/ex00/templates.hpp
@@ -0,0 +1,26 @@
+#ifndef T_H
+# define T_h
+# include <iostream>
+
+template <typename T>
+void swap(T a, T b)
+{
+    T c;
+    c = a;
+    a = b;
+    a = c;
+}
+
+template <typename T2>
+T2 min(T2 a, T2 b)
+{
+    return (a < b) ? a : b;
+}
+
+template <typename T3>
+T3 max(T3 a, T3 b)
+{
+    return (a < b) ? b : a;
+}
+
+#endif
--- cpp07/ex00/test.cpp
+++ cpp07/ex00/test.cpp
@@ -2,21 +2,30 @@
 #include <string>
 using namespace std;
 
-int add(int x, int y)
+
+template <typename T>
+int max_(T x, int y=1)
 {
-    cout<<"heeeeey from int"<<endl;
-    return (0);
+    cout<<"waaaaaaaaaa"<<sizeof(x)<<" "<<sizeof(y)<<endl;
+    return (x < y) ? y : x;
 }
 
-double add(double x, double y)
-{
-    cout<<"heeeeeeey from double"<<endl;
-    return (0);
-}
+// int max(int x, int y)
+// {
+//     cout<<"hello from int yo"<<endl;
+//     return (x < y) ? y : x;
+// }
+
+// double max_(double x, double y)
+// {
+//     cout<<"hello from double"<<endl;
+//     return (x < y) ? y : x;
+// }
 
 int main(int argc, char const *argv[])
 {
-    double a, b;
-    add('a', 'b');
+    double a = 5.6, b = 5.4;
+    double a1 = 5, b1 = 6;
+    cout<<sizeof(max_(5, b1))<<endl;
     return (0);
 }
