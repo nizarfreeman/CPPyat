b73a034cc37053a137f4756fdac151045e9b8dab
nizarfreeman <gormarduck2005@gmail.com>
2026-04-29 10:49:01 +0100

    nf

--- cpp07/ex02/Array.hpp
+++ cpp07/ex02/Array.hpp
@@ -1,21 +1,51 @@
-#ifndef ARRAY_
-# define ARRAY_
+#ifndef A_H
+# define A_H
 # include <iostream>
+# include <stdexcept>
 
 template <typename T>
 class Array
 {
     private:
-        T *arr;
-        size_t len;
+        T* arr;
+        unsigned int size_;
     public:
-        Array() : arr(new T[0]()), len(0) {}
-        Array(unsigned int n): arr(new T[n]()) len(n) {}
-        Array(const Array *other)
+        Array(): arr(NULL), size_(0) {}
+        Array(unsigned int n): arr(new T[n]()), size_(n) {}
+        Array(const Array& other): arr(new T[other.size_]()), size_(other.size_)
         {
-            
+            for (size_t i = 0; i < size_; i++)
+                arr[i] = other.arr[i];
         }
+        Array& operator=(const Array& other)
+        {
+            if (this != &other)
+            {
+                delete[] arr;
+                size_ = other.size_;
+                arr = new T[size_]();
+                for (size_t i = 0; i < size_; i++)
+                    arr[i] = other.arr[i];
+            }
+            return *this;
+        }
+        ~Array() { delete[] arr; }
 
+        unsigned int size() const { return (size_); }
+
+        T& operator[](unsigned int index)
+        {
+            if (index >= size_ )
+                throw std::out_of_range("Index out of range");
+            return arr[index];
+        }
+        
+        const T& operator[](unsigned int index) const
+        {
+            if (index >= size_)
+                throw std::out_of_range("Index out of range");
+            return arr[index];
+        }
 };
 
 #endif
