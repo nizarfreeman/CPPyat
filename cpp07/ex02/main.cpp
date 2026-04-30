6148bd0c59955122c5f98163e1a8a3d9b4503468
Nizar Faska <nfaska@e1r6p9.1337.ma>
2026-04-30 15:47:47 +0100

    nf

--- cpp07/ex02/main.cpp
+++ cpp07/ex02/main.cpp
@@ -1,53 +1,71 @@
+#include <iostream>
 #include "Array.hpp"
-#include <ctime>
-#include <cstdlib>
-#define MAX_VAL 750
-int main(int, char**)
+
+class A
 {
-    Array<int> numbers(MAX_VAL);
-    int* mirror = new int[MAX_VAL];
-    srand(time(NULL));
-    for (int i = 0; i < MAX_VAL; i++)
-    {
-        const int value = rand();
-        numbers[i] = value;
-        mirror[i] = value;
-    }
-    //SCOPE
-    {
-        Array<int> tmp = numbers;
-        Array<int> test(tmp);
-    }
+    public:
+        int x;    
+        A() : x(0) {}
+        A(int v) : x(v) {}
+};
 
-    for (int i = 0; i < MAX_VAL; i++)
-    {
-        if (mirror[i] != numbers[i])
-        {
-            std::cerr << "didn't save the same value!!" << std::endl;
-            return 1;
-        }
-    }
+std::ostream& operator<<(std::ostream& o, const A& a)
+{
+    o<<a.x;
+    return o;
+}
+
+int main()
+{
+    std::cout<<"empty test\n";
     try
     {
-        numbers[-2] = 0;
+        Array<int> e;
+        std::cout<<e.size()<<"\n";
+        std::cout<<e[0]<<"\n"; // throws
     }
-    catch(const std::exception& e)
+    catch (std::exception& e)
     {
-        std::cerr << e.what() << '\n';
-    }
-    try
-    {
-        numbers[MAX_VAL] = 0;
-    }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << '\n';
+        std::cout<<e.what()<<"\n";
     }
 
-    for (int i = 0; i < MAX_VAL; i++)
+    std::cout<<"\nint test\n";
+    Array<int> a(3);
+    for (unsigned int i = 0; i < a.size(); i++)
+        a[i] = i + 1;
+    for (unsigned int i = 0; i < a.size(); i++)
+        std::cout<<a[i]<<" ";
+    std::cout<<"\n";
+
+    std::cout<<"\nout of range\n";
+    try
     {
-        numbers[i] = rand();
+        std::cout<<a[10]<<"\n"; // throws
     }
-    delete [] mirror;//
+    catch (std::exception& e)
+    {
+        std::cout<<e.what()<<"\n";
+    }
+
+    std::cout<<"\ncopy test\n";
+    Array<int> b = a;
+    for (unsigned int i = 0; i < b.size(); i++)
+        std::cout<<b[i]<<" ";
+    std::cout<<"\n";
+
+    std::cout<<"\nconst test\n";
+    const Array<int> c(a);
+    for (unsigned int i = 0; i < c.size(); i++)
+        std::cout<<c[i]<<" ";
+    std::cout<<"\n";
+
+    std::cout<<"\nclass test\n";
+    Array<A> d(3);
+    for (unsigned int i = 0; i < d.size(); i++)
+        d[i] = A(i + 10);
+    for (unsigned int i = 0; i < d.size(); i++)
+        std::cout<<d[i]<<" ";
+    std::cout<<"\n";
+
     return 0;
 }
