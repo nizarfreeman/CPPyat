05ea36e6b3083ff404d14ea04b361f46315c0b47
Nizar Faska <nfaska@e1r5p7.1337.ma>
2026-05-03 19:08:29 +0100

    nf

--- cpp08/ex00/main.cpp
+++ cpp08/ex00/main.cpp
@@ -1,21 +1,52 @@
 #include "easyfind.hpp"
 #include <vector>
+#include <list>
+#include <stack>
+#include <deque>
 
 int main(void)
 {
-    std::vector<double> dbl_vec;
+    std::vector<int> dbl_vec;
     dbl_vec.push_back(10);
     dbl_vec.push_back(20);
     dbl_vec.push_back(30);
-
     try
     {
-        std::vector<double>::iterator it =  easyfind(dbl_vec, 10);
+        std::vector<int>::iterator it =  easyfind(dbl_vec, 10);
         std::cout<<*it<<std::endl;
     }
     catch(const std::exception& e)
     {
         std::cerr<<e.what()<<'\n';
+    }
+
+    std::list<int> str_l;
+    str_l.push_front(10);
+    str_l.push_front(20);
+    str_l.push_front(30);
+    try
+    {
+        std::list<int>::iterator it = easyfind(str_l, 40);
+        std::cout<<*it<<std::endl;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr<<e.what()<<'\n';
+    }
+    
+    std::deque<int> qu_;
+    qu_.push_back(10);
+    qu_.push_back(20);
+    qu_.push_back(30);
+    try
+    {
+        std::deque<int>::iterator it =  easyfind(qu_, 10);
+        std::cout<<*it<<std::endl;
+        it = easyfind(qu_, 50);
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
     }
     
     
